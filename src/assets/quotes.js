687868c6114235d8ca8466b9a7a17fff9c2383ba
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-06-12 15:38:32 -0500

    Fixed format of copied quote

--- src/assets/quotes.js
+++ src/assets/quotes.js
@@ -1,36 +1,27 @@
 const quotes = {
     0: {
         name: `Ta-Nehisi Coates`,
-        text: `What sets black people apart is not some deficit in personal responsibility. 
-               It's the weight on our shoulders. That is what's actually different. 
-               We have the weight and burden of history.` 
+        text: `What sets black people apart is not some deficit in personal responsibility. It's the weight on our shoulders. That is what's actually different. We have the weight and burden of history.` 
         },
     1: {
         name: `Ta-Nehisi Coates`,
-        text: `Mistakes were made. Bodies were broken. People were enslaved. 
-               We meant well. We tried our best. “Good intention” is a hall pass 
-               through history, a sleeping pill that ensures the Dream.` 
+        text: `Mistakes were made. Bodies were broken. People were enslaved. We meant well. We tried our best. “Good intention” is a hall pass through history, a sleeping pill that ensures the Dream.` 
         },
     2: {
         name: `Robin DiAngelo`,
-        text: `It is white people’s responsibility to be less fragile; people 
-               of color don’t need to twist themselves into knots trying to navigate 
-               us as painlessly as possible.` 
+        text: `It is white people’s responsibility to be less fragile; people of color don’t need to twist themselves into knots trying to navigate us as painlessly as possible.` 
         },
     3: {
         name: `Robin DiAngelo`,
-        text: `All systems of oppression are adaptive; they can withstand and adjust 
-               to challenges and still maintain inequality.` 
+        text: `All systems of oppression are adaptive; they can withstand and adjust to challenges and still maintain inequality.` 
         },
     4: {
         name: `Robin DiAngelo`,
-        text: `It’s been said that racism is so American that when we protest racism, 
-               some assume we’re protesting America.` 
+        text: `It’s been said that racism is so American that when we protest racism, some assume we’re protesting America.` 
         },
     5: {
         name: `Ibram X. Kendi`,
-        text: `Like fighting an addiction, being an antiracist requires persistent self-awareness, 
-               constant self-criticism, and regular self-examination.` 
+        text: `Like fighting an addiction, being an antiracist requires persistent self-awareness, constant self-criticism, and regular self-examination.` 
         }
 }
 
